d33245517620e59af43484f9d6e22877ad70d464
j7ng <40279769+j7ng@users.noreply.github.com>
2018-07-26 14:20:57 -0400

    Delete ACT_SP_INACT_PI3.sql

--- SA/Tables/ACT_SP_INACT_PI3.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE sa.act_sp_inact_pi3 (
-  x_service_id VARCHAR2(30 BYTE),
-  x_min VARCHAR2(30 BYTE)
-);
-ALTER TABLE sa.act_sp_inact_pi3 ADD SUPPLEMENTAL LOG GROUP dmtsora661755677_0 (x_min, x_service_id) ALWAYS;
