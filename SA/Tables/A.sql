dc2e83b1923d14cf88d8fafced718e5aeed61fe0
j7ng <40279769+j7ng@users.noreply.github.com>
2018-08-01 12:06:10 -0400

    Update A.sql

--- SA/Tables/A.sql
+++ SA/Tables/A.sql
@@ -1,4 +1,4 @@
 CREATE TABLE sa."A" (
-  "NAME" VARCHAR2(100 BYTE), date_mod date default sysdate
+  "NAME" VARCHAR2(100 BYTE), date_mod date default sysdate, JT varchar2(30) default "JT"
 );
 ALTER TABLE sa."A" ADD SUPPLEMENTAL LOG GROUP dmtsora1036313094_0 ("NAME") ALWAYS;
